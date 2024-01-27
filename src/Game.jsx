fea7ffcf4fbe5872e9cb6a91848d43faecf8cb84
noel-pena <azulu88@hotmail.com>
2024-01-27 13:05:08 -0500

    click

--- src/Game.jsx
+++ src/Game.jsx
@@ -23,7 +23,43 @@ export const Game = () => {
     }
   }
 
+  //Game sequence
+  function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    let randomNum = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNum];
+    gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor)
+      .fadeIn(100)
+      .fadeOut(100)
+      .fadeIn(100);
+    playSound(randomChosenColor);
+  }
+
   //Click functionality
+  $(".btn").click(function () {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+  });
+
+  //Sound functionality
+  function playSound(name) {
+    let audio = new Audio("public/sounds/" + name + ".mp3");
+    audio.play();
+  }
+
+  //Button animation
+  function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+  }
 
   return (
     <React.Fragment>
